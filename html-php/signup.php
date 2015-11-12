dd2913cbdb806c4c8be704376e0b0db5bcb8cb42
SvrAsura <silveryasura@naver.com>
2015-11-12 22:22:03 +0900

    11-12 / 10:21
    첫번째 보고(?)

---
+++ html-php/signup.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+	<body>
+    <font size="5" color="900090">
+    	<table border="1" align="center">
+        	<tr><td><font size="9">회원가입</font></td></tr>
+        	<tr><td> r g b 2</td></tr>
+        	<tr><td>3</td></tr>
+        	<tr><td>4</td></tr>
+        	<tr><td>5</td></tr>
+        	<tr><td>6</td></tr>
+        	<tr><td>7</td></tr>
+        	<tr><td>8</td></tr>
+        	<tr><td>9</td></tr>
+        	<tr><td>0</td></tr>
+        </table>
+	</body>
+</html>
