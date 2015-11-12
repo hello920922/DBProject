008dc5a84cb7ee9cbd0a3e895bbe589f598bc147
SvrAsura <silveryasura@naver.com>
2015-11-12 23:34:50 +0900

    11-12 / 11:34
    세번째

--- html-php/signup.php
+++ html-php/signup.php
@@ -8,31 +8,64 @@
 	<body>
     <font size="5" color="900090">
     	<table border="1" align="center">
-        	<tr><td><font size="9">회원가입</font></td></tr>
-        	<tr><td> r g b 2</td></tr>
-        	<tr><td><table>
-                    	<tr><td>PassWord</td><td><input type="passwd"></td></tr>
-            </table></td></tr>
+        
+        	<tr><td align="center"><font size="9">회원가입</font></td></tr>
             
-        	<tr><td><table>
-                    	<tr><td>Name</td><td><input type="text"></td></tr>
-            </table></td></tr>
+        	<tr><td>
+            	<table>
+                    <tr><td>ID :</td><td width="176"><input type="text" maxlength="15" size="15"></td></tr>
+            	</table>
+            </td></tr>
             
-        	<tr><td><table>
-                    	<tr><td>주민등록번호</td><td><input type="text"></td></tr>
-            </table></td></tr>
+        	<tr><td>
+            	<table>
+                    	<tr><td>PassWord :</td><td><input type="password" maxlength="15" size="15"></td></tr>
+           		</table>
+            </td></tr>
             
-        	<tr><td><table>
-                    	<tr><td>핸드폰</td><td><input type="text"></td></tr>
-            </table></td></tr>
+        	<tr><td>
+            	<table>
+                    	<tr><td>Name :</td><td><input type="text" maxlength="30" size="30"></td></tr>
+            	</table>
+            </td></tr>
             
-        	<tr><td><table>
-                    	<tr><td>E-Mail</td><td><input type="text"></td></tr>
-            </table></td></tr>
+        	<tr><td>
+            	<table>
+                    	<tr><td>주민등록번호 :</td><td><input type="text" maxlength="6" size="6">-<input type="password" maxlength="7" size="7"></td></tr>
+            	</table>
+            </td></tr>
             
-        	<tr><td><table>
-                    	<tr><td><input type="submit" value="가입하기"></td></tr>
-            </table></td></tr>
+        	<tr><td>
+            	<table>
+                    	<tr><td>Phone :</td><td><input type="text" maxlength="3" size="3">-<input type="text" maxlength="4" size="4">-<input type="text" maxlength="4" size="4"></td></tr>
+            	</table>
+            </td></tr>
+            
+        	<tr><td>
+            	<table>
+                    	<tr><td>E-Mail :</td>
+                        <td>
+                        	<input type="text" maxlength="30" size="30">@<select name="selct">
+                       		<option value="">이메일 선택</option>
+    						<option value="">naver.com</option>
+    						<option value="">daum.net</option>
+    						<option value="">gmail.com</option>
+    						<option value="">nate.com</option>
+    						<option value="">dreamwiz.com</option>
+    						<option value="">korea.com</option>
+    						<option value="">outlook.com</option>
+    						<option value="">기타</option>
+                         </td></tr>
+            	</table>
+            </td></tr>
+            
+        	<tr><td>
+            	<table align="center">
+                    	<tr><td><input type="submit" value="가입하기" ></td></tr>
+            	</table>
+            </td></tr>
+            
+            
             
         </table>
 	</body>
