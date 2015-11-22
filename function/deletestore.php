b4a2c8c743dba9539f3baa1a6f490925a225475b
Mingyu Park <hello920922@gamil.com>
2015-11-22 23:20:24 +0900

    add delete store function file

---
+++ function/deletestore.php
@@ -0,0 +1,24 @@
+<?php
+    session_start();
+
+    if(!isset($_SESSION['id']) || !isset($_SESSION['name']))
+        echo "<script> location.replace(\"./\"); </script>";
+
+    else{
+        $license = $_POST['license'];
+
+        include("dbconnect.php");
+
+        $query  = "delete from STORE where LICENSE='".$license."'";
+
+            $result = executeQuery($conn, $query);
+
+            if(!$result){
+                echo "<script> alert(\"Delete Failure\"); </script>";
+                echo "<script> history.back(); </script>";
+            }
+
+            echo "<script> alert(\"The store is deleted successful\"); </script>";
+            echo "<script> location.replace(\"../\"); </script>";
+    }
+?>
