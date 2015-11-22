5a6b41f0e67aeaf627f4d055c77629b890769aa3
Mingyu Park <hello920922@gamil.com>
2015-11-22 23:19:40 +0900

    add delete menu function file

---
+++ function/deletemenu.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+
+    if(!isset($_SESSION['id']) || !isset($_SESSION['name']))
+        echo "<script> location.replace(\"./\"); </script>";
+
+    else{
+        $license = $_POST['license'];
+        $item = $_POST['item'];
+
+        include("dbconnect.php");
+
+        $query  = "delete from MENU where LICENSE='".$license."' and ITEM='".$item."'";
+
+        $result = executeQuery($conn, $query);
+
+        if(!$result){
+            echo "<script> alert(\"Delete Failure\"); </script>";
+            echo "<script> history.back(); </script>";
+        }
+
+        echo "<script> alert(\"The menu is deleted successful\"); </script>";
+        echo "<script> location.replace(\"../\"); </script>";
+    }
+?>
