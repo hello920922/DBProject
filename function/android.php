38784cda399f98592dfe13dd70ecbd180fdb74b5
Mingyu Park <hello920922@gamil.com>
2015-11-27 03:30:44 +0900

    update android.php

--- function/android.php
+++ function/android.php
@@ -26,9 +26,9 @@
 
         $result = selectQuery($conn,$query);
         if($result != null){
-            $cnt = count($result);
+            $length = count($result);
 
-            for($i=0; $i<$cnt; $i++){
+            for($i=0; $i<$length; $i++){
                 echo $result[$i]['SNAME'].",";
                 echo $result[$i]['GRADE'].",";
                 echo $result[$i]['NOTE'].",";
@@ -116,6 +116,130 @@
             }
         }
         exit;
+    }
+    else if(!strcmp($func,"menudetail")){
+        include("dbconnect.php");
+        $item = $_POST['item'];
+        $license = $_POST['license'];
+
+        $query  = "select SNAME, PRICE, MENU.IMG from MENU, STORE where ";
+        $query .= "MENU.LICENSE='".$license."' and ";
+        $query .= "ITEM='".$item."' and ";
+        $query .= "MENU.LICENSE=STORE.LICENSE";
+
+
+        $result = selectQuery($conn, $query);
+        if($result!=null){
+            echo $result[0]['SNAME'].",";
+            echo $result[0]['PRICE'].",";
+            echo $result[0]['IMG'];
+            exit;
+        }
+    }
+    else if(!strcmp($func,"reviewdetail")){
+        include("dbconnect.php");
+
+        $cid = $_POST['cid'];
+        $license = $_POST['license'];
+
+        $query  = "select SNAME, ADDR, GRADE, NOTE, DATE ";
+        $query .= "from REVIEW, STORE where ";
+        $query .= "CID='".$cid."' and ";
+        $query .= "REVIEW.LICENSE='".$license."' and ";
+        $query .= "REVIEW.LICENSE=STORE.LICENSE";
+
+        $result = selectQuery($conn, $query);
+        if($result!=null){
+            echo $result[0]['SNAME'].",";
+            echo $result[0]['ADDR'].",";
+            echo $result[0]['GRADE'].",";
+            echo $result[0]['NOTE'].",";
+            echo $result[0]['DATE'];
+            exit;
+        }
+    }
+    else if(!strcmp($func,"morereview")){
+        include("dbconnect.php");
+        $license = $_POST['license'];
+
+        $query  = "select SNAME from STORE where ";
+        $query .= "LICENSE='".$license."'";
+
+        $store = selectQuery($conn, $query);
+        if($store!=null){
+            $query  = "select GRADE, NOTE, CID, DATE from REVIEW where ";
+            $query .= "LICENSE='".$license."'";
+
+            $review = selectQuery($conn, $query);
+            if($review!=null){
+                echo $store[0]['SNAME']."/nextResult/";
+                $length = count($review);
+                for($i=0; $i<$length; $i++){
+                    echo $review[$i]['GRADE'].",";
+                    echo $review[$i]['NOTE'].",";
+                    echo $review[$i]['CID'].",";
+                    echo $review[$i]['DATE']."/";
+                }
+                exit;
+            }
+        }
+    }
+    else if(!strcmp($func,"account")){
+        include("dbconnect.php");
+        $cid = $_POST['cid'];
+
+        $query  = "select NAME, BIRTH, GENDER, PHONE, EMAIL from ";
+        $query .= "CUSTOMER where ";
+        $query .= "CID='".$cid."'";
+
+        $result = selectQuery($conn, $query);
+        if($result!=null){
+            echo $result[0]['NAME'].",";
+            echo $result[0]['BIRTH'].",";
+            echo $result[0]['GENDER'].",";
+            echo $result[0]['PHONE'].",";
+            echo $result[0]['EMAIL'];
+        }
+        exit;
+    }
+    else if(!strcmp($func,"changeacc")){
+        include("dbconnect.php");
+
+        $cid = $_POST['cid'];
+        $passwd = $_POST['passwd'];
+        $name = $_POST['name'];
+        $birth = $_POST['birth'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+        $newpasswd = $_POST['newpasswd'];
+
+        $query  = "select * from CUSTOMER where ";
+        $query .= "CID='".$cid."' and PASSWORD='".$passwd."'";
+
+        $result = selectQuery($conn, $query);
+        if($result==null){
+            echo "WRONG";
+            exit;
+        }
+
+        $query  = "update CUSTOMER set ";
+        $query .= "NAME='".$name."', ";
+        $query .= "BIRTH='".$birth."', ";
+        $query .= "PHONE='".$phone."', ";
+
+        if($newpasswd!=null){
+            $query .= "PASSWORD='".$newpasswd."', ";
+        }
+
+        $query .= "EMAIL='".$email."' where ";
+        $query .= "CID='".$cid."'";
+
+        $result = executeQuery($conn, $query);
+
+        if($result){
+            echo "SUCCESS";
+            exit;
+        }
     }
     echo "ERROR";
 ?>
